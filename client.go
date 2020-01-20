a324db60c1a119a60f02e9203f2e870249cb1718
sihadan <def6488@gmail.com>
2020-01-20 16:12:00 +0900

    implement search train

--- client.go
+++ client.go
@@ -87,13 +87,13 @@ func (c *Client) Logout() error {
 // SearchTrain is used to search trains from SRT server,
 // SearchTrain returns *only* available seats,
 // if you want to retrieve trains with no seats as well, Use SearchTrainAll() instead
-func (c *Client) SearchTrain(dep, arr, date, time string) ([]*Train, error) {
+func (c *Client) SearchTrain(dep, arr, date, depTime string) ([]*Train, error) {
 	// TODO filtering
-	return c.SearchTrainAll(dep, arr, date, time)
+	return c.SearchTrainAll(dep, arr, date, depTime)
 }
 
 // SearchTrainAll is used to search *all* trains including trains with no seats
-func (c *Client) SearchTrainAll(dep, arr, date, time string) ([]*Train, error) {
+func (c *Client) SearchTrainAll(dep, arr, date, depTime string) ([]*Train, error) {
 	if !c.isLogin {
 		return nil, errors.New("Not Loggin In")
 	}
@@ -107,27 +107,29 @@ func (c *Client) SearchTrainAll(dep, arr, date, time string) ([]*Train, error) {
 		return nil, fmt.Errorf("Station `%s` Not Exists", arr)
 	}
 
+	formData := map[string]string{
+		// course (1: 직통, 2: 환승, 3: 왕복)
+		// TODO: support 환승, 왕복
+		"chtnDvCd":   "1",
+		"arriveTime": "N",
+		"seatAttCd":  "015",
+		// 검색 시에는 1명 기준으로 검색
+		"psgNum":  "1",
+		"trnGpCd": "109",
+		// train type (05: 전체, 17: SRT)
+		"stlbTrnClsfCd": "05",
+		// departure date
+		"dptDt": date,
+		// departure time
+		"dptTm": depTime,
+		// arrival station code
+		"arvRsStnCd": arrCode,
+		// departure station code
+		"dptRsStnCd": depCode,
+	}
+
 	resp, err := c.httpClient.R().
-		SetFormData(map[string]string{
-			// course (1: 직통, 2: 환승, 3: 왕복)
-			// TODO: support 환승, 왕복
-			"chtnDvCd":   "1",
-			"arriveTime": "N",
-			"seatAttCd":  "015",
-			// 검색 시에는 1명 기준으로 검색
-			"psgNum":  "1",
-			"trnGpCd": "109",
-			// train type (05: 전체, 17: SRT)
-			"stlbTrnClsfCd": "05",
-			// departure date
-			"dptDt": date,
-			// departure time
-			"dptTm": time,
-			// arrival station code
-			"arvRsStnCd": arrCode,
-			// departure station code
-			"dptRsStnCd": depCode,
-		}).
+		SetFormData(formData).
 		Post(srtSearchScheduleURL)
 
 	if err != nil {
@@ -171,7 +173,38 @@ func (c *Client) SearchTrainAll(dep, arr, date, time string) ([]*Train, error) {
 		trains = append(trains, toTrain(t.(map[string]interface{})))
 	}
 
-	// TODO: pagination
+	// Note: updated api uses pagination,
+	//      therefore, to retreive all trains, retry searching unless there are no more trains
+	for len(trains) > 0 {
+		nextDepTime := tickSecond(trains[len(trains)-1].depTime)
+		formData["dptTm"] = nextDepTime
+
+		resp, err := c.httpClient.R().
+			SetFormData(formData).
+			Post(srtSearchScheduleURL)
+
+		if err != nil {
+			return nil, err
+		}
+
+		parser := &responseParser{}
+		err = parser.Parse(resp.Body())
+
+		if err != nil {
+			return nil, err
+		}
+
+		if !parser.Success() {
+			break
+		}
+
+		trainsData := parser.
+			Data()["outDataSets"].(map[string]interface{})["dsOutput1"].([]interface{})
+
+		for _, t := range trainsData {
+			trains = append(trains, toTrain(t.(map[string]interface{})))
+		}
+	}
 
 	return trains, nil
 }
--- utils.go
+++ utils.go
@@ -1,5 +1,10 @@
 package srt
 
+import (
+	"fmt"
+	"strconv"
+)
+
 func reverseMap(m map[string]string) map[string]string {
 	_m := map[string]string{}
 	for k, v := range m {
@@ -7,4 +12,26 @@ func reverseMap(m map[string]string) map[string]string {
 	}
 
 	return _m
+}
+
+// tick one second (HHMMSS --> HHMMSS + 1 sec)
+func tickSecond(s string) string {
+	hour, _ := strconv.Atoi(s[0:2])
+	minute, _ := strconv.Atoi(s[2:4])
+	second, _ := strconv.Atoi(s[4:6])
+
+	second++
+	if second == 60 {
+		second = 0
+		minute++
+	}
+	if minute == 60 {
+		minute = 0
+		hour++
+	}
+	if hour == 24 {
+		hour = 0
+	}
+
+	return fmt.Sprintf("%.2d%.2d%.2d", hour, minute, second)
 }
